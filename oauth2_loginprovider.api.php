7089c450a5b7ce54b5e346c1d23117dca63fed3e
Dashamir Hoxha <dashohoxha@gmail.com>
2017-08-24 15:17:55 +0200

    Add drush commands for managing oauth2_server clients.

--- oauth2_loginprovider.api.php
+++ oauth2_loginprovider.api.php
@@ -21,32 +21,26 @@ function MODULE_oauth2_loginprovider_userprofile_alter(&$data) {
  * Implements hook_install().
  */
 function PROFILE_install() {
-  global $base_url;
-  $server_name = 'oauth2';
   $client_key = 'client1';
   $client_secret = md5(uniqid(rand(), TRUE));
-  $redirect_uri = $base_url . '/oauth2/authorized';
+  $redirect_uri = $GLOBALS['base_url'] . '/oauth2/authorized';
+  oauth2_client_add($client_key, $client_secret, $redirect_uri);
+}
 
-  // Delete the client if already exists.
-  $query = new EntityFieldQuery();
-  $clients = $query->entityCondition('entity_type', 'oauth2_server_client')
-    ->propertyCondition('server', $server_name)
-    ->propertyCondition('client_key',  $client_key)
-    ->execute();
-  if (isset($clients['oauth2_server_client'])) {
-    $cids = array_keys($clients['oauth2_server_client']);
-    foreach ($cids as $cid) {
-      entity_delete('oauth2_server_client', $cid);
-    }
-  }
+/**
+ * Create a new oauth2_server client.
+ */
+function oauth2_client_add($client_key, $client_secret, $redirect_uri) {
+}
 
-  // Register a client on the oauth2 server.
-  $client = entity_create('oauth2_server_client', array());
-  $client->server = $server_name;
-  $client->label = 'Test OAuth2 Client';
-  $client->client_key = $client_key;
-  $client->client_secret = $client_secret;
-  $client->redirect_uri = $redirect_uri;
-  $client->automatic_authorization = TRUE;
-  $client->save();
+/**
+ * Delete the given client if exists.
+ */
+function oauth2_client_del($client_key) {
+}
+
+/**
+ * Get a list of oauth2_server clients.
+ */
+function oauth2_client_list() {
 }
